c187d22598254be9eb8f51c1879d7ab3f677942d
tejaswinireddy12 <godhatejaswini12@gmail.com>
2025-06-05 12:02:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactUs from './ContactUs';
+
+function App() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
